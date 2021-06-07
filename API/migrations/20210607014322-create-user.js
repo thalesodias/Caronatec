ed01f5c3c6ac23c72a8f648622a23cc5daefbdd9
matheusnog <matheusnogueira450@gmail.com>
2021-06-06 22:53:36 -0300

    API iniciada

---
+++ API/migrations/20210607014322-create-user.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      profile_type: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {        
+        type: Sequelize.DATE
+      },
+      updatedAt: {        
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
